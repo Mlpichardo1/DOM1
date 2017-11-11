07213546998e008bbb804f86753478ca3752e778
Manuel Pichardo <mlpichardo1@gmail.com>
2017-11-11 18:37:37 +0000

    Object functions

---
+++ cars/objects1.js
@@ -0,0 +1,41 @@
+   var cars = { 
+	 name: ["Economy", "Midsize"],
+	 economyCars: 125,
+	 economyCarsBooked: 0,
+	 midsizeCars: 100,
+	 midsizeCarsBooked: 0,
+	 
+	  ecarFunction: function() {
+	   return this.economyCars - this.economyCarsBooked;
+	  },
+	  
+	  midcarFunction: function() {
+	   return this.midsizeCars - this.midsizeCarsBooked;
+	  },
+	  
+	  bookEcon: function() {
+	      return this.economyCarsBooked++;
+	  },
+	  
+	  bookMid: function() {
+	      return this.midsizeCarsBooked++;
+	  }
+	  };
+	    
+	function availFunction() {
+	document.getElementById("ecars").innerHTML = cars.name[0];
+	document.getElementById("midcars").innerHTML = cars.name[1];
+	document.getElementById("ecarsAvail").innerHTML = cars.ecarFunction();
+	document.getElementById("midcarsAvail").innerHTML = cars.midcarFunction();
+	}
+	
+	function rentCar() {
+	cars.bookEcon();
+	document.getElementById("ecarsAvail").innerHTML = cars.ecarFunction();
+	}
+
+    function rentMidcar(){
+    cars.bookMid();
+	document.getElementById("midcarsAvail").innerHTML = cars.midcarFunction();    
+    }
+	
